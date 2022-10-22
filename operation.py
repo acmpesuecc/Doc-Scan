bef0a663022470b7f2408b1e7de7bc50245e79ad
ad1tya24 <adityasiddanur@gmail.com>
2022-10-22 07:55:17 +0530

    Comparing ppt, videos, excel file added

---
+++ operation.py
@@ -0,0 +1,11 @@
+def switch(Op):
+    if Op == "JPG":
+        return "You can become a web developer."
+    elif Op == "PPT":
+        return "You can become a backend developer."
+    elif Op == "TEXT":
+        return "You can become a Data Scientist"
+    elif Op == "Video":
+        return "You can become a Blockchain developer."
+    elif Op == "Excel":
+        return "You can become a mobile app developer"
